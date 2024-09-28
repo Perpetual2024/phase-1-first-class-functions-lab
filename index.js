c3e28958f2350ff777787e0504e27a588bea8ed4
Perpetual Akinyi <you@example.com>
2024-09-28 22:03:53 +0300

    updates

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+return drivers.slice(0, 2)
+}
+function returnLastTwoDrivers(drivers){
+return drivers.slice(2, 4)
+}
+const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(multiplier) {
+    return function(value){
+        return value * multiplier;
+    }
+}
+function fareDoubler(fares){
+    return fares * 2;
+}
+function fareTripler(fares){
+    return fares * 3;
+}
+function selectDifferentDrivers(arrayOfDrivers,  selectingDrivers){
+    return selectingDrivers(arrayOfDrivers);
+}
+
